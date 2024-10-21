d8081973796e5d6a28f754de850cd676599dfc9c
mrWinnee <yassinih490@gmail.com>
2024-10-21 19:54:48 +0100

    update origin

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const server = require('http').createServer(app)
 const {Server} = require('socket.io')
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://online-zerbiya-game.onrender.com/",
         methods: ["GET", "POST"],
     },
 })
