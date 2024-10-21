7a8fe888be8b243201a431082295d8b7c1429904
mrWinnee <yassinih490@gmail.com>
2024-10-21 19:52:35 +0100

    fixing cors issue

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
 const {Server} = require('socket.io')
-const cors = require(cors);
 const io = new Server(server, {
     cors: {
         origin: "http://localhost:3000",
