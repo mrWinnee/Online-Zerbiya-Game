a33cb3a6498cc85917d40cde6fdd3252b7c44856
mrWinnee <yassinih490@gmail.com>
2024-10-26 11:23:30 +0100

    fixing path issue

--- server.js
+++ server.js
@@ -240,7 +240,7 @@ io.on('connection', (socket) => {
 
 if(process.env.NODE_ENV === "production"){
     app.get('*', (req, res)=>{
-        res.sendFile(__dirname + './index.html')
+        res.sendFile(__dirname + '/index.html')
     })
 }
 
